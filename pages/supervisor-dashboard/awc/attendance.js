f7621000243e80d31a5e29d1a789251e2aa853cb
smdsaran <saranthuppaki056@gmail.com>
2022-04-20 12:47:04 +0530

    added appbr and footer in superpage attendance & stock for avoid err in vercel build

---
+++ pages/supervisor-dashboard/awc/attendance.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AppBarMuiForSupervisor from "../../../components/Layout/AppBarMuiForSupervisorInsideAWC";
+import Footer from "../../../components/Layout/Footer";
+
+const attendance = () => {
+  return (
+    <div>
+      <AppBarMuiForSupervisor />
+      <Footer />
+    </div>
+  );
+};
+
+export default attendance;
---
+++ pages/supervisor-dashboard/awc/stock-management.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AppBarMuiForSupervisor from "../../../components/Layout/AppBarMuiForSupervisorInsideAWC";
+import Footer from "../../../components/Layout/Footer";
+
+const stock = () => {
+  return (
+    <div>
+      <AppBarMuiForSupervisor />
+      <Footer />
+    </div>
+  );
+};
+
+export default stock;
