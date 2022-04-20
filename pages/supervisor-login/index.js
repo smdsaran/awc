a2b73606332924f5c13325d4a40f0c5ce1cba10c
smdsaran <saranthuppaki056@gmail.com>
2022-04-20 12:28:40 +0530

    changed eslint

--- pages/supervisor-login/index.js
+++ pages/supervisor-login/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import UserLogin from "../../components/Login/SupervisorLogin";
 import Appbar from "../../components/Layout/AppBarForAWW";
 import Footer from "../../components/Layout/Footer";
