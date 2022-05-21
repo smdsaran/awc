e25a066fa66b80efb85cf083f3c0f08023dd4642
smdsaran <saranthuppaki056@gmail.com>
2022-05-21 21:41:08 +0530

    changed Attendance to AWWs Attendance

--- components/Supervisor/Attendance.js
+++ components/Supervisor/Attendance.js
@@ -46,7 +46,7 @@ const Attendance = () => {
   return (
     <div>
       <div className="text-center bg-purple-700 text-3xl py-4 text-white mb-8">
-        Attendance
+        AWWs Attendance
       </div>
 
       <DropDownAttendance selected={selectedDays} />
